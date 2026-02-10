649a3110ab58d60c4d0c1e76582f11d6c088ddf8
Developer-Sohail786 <sanu30963@gmail.com>
2026-02-10 19:58:56 +0530

    Refactor App component layout and formatting

--- Chart Vision/src/App.jsx
+++ Chart Vision/src/App.jsx
@@ -1,19 +1,15 @@
-import React from "react"
-import { Outlet } from "react-router-dom"
-
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  
-
   return (
-    <>
-<div className="min-h-screen bg-gray-800 text-white">
-  <main className=" p-6">
-    <Outlet />
-  </main>
-</div>
-    </>
-  )
+    <div className="min-h-screen bg-gray-800 text-white flex flex-col">
+      <main className="flex-1 p-4 sm:p-6">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
